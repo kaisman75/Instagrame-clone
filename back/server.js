7d03068b660d095b806f6003d7ef3d4c25290d6f
kais manai <kaismanai1975@gmail.com>
2021-11-17 12:24:44 +0100

    add DB

--- back/server.js
+++ back/server.js
@@ -9,12 +9,15 @@ import Pusher from "pusher";
 const app=express();
 const port= process.env.PORT||8080
 // Middlewares
-app.use(express.json());
+app.use(express.json()); 
 app.use(cors());
 
 //DB config
-
-
+const connection_url="mongodb+srv://dbKais:111919manai@cluster0.i7omy.mongodb.net/instagrameClone?retryWrites=true&w=majority"
+mongoose.connect(connection_url);
+mongoose.connection.once("open",()=>{
+    console.log("DB connected")
+})
 // api routes
 app.get("/",(req,res)=>res.status(200).send("Hello world"))
    
